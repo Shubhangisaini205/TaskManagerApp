8f94921ba214f28c5493703e41c5015f887d8cbe
Shubhangi Saini <115461618+Shubhangisaini205@users.noreply.github.com>
2024-10-30 00:07:02 +0530

    Update backend  deployed api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://taskmanagerapp-qc92.onrender.com/api";
 
 export const registerUser = async (userData) => {
     console.log(userData)
